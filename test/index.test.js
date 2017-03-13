8ace111165451ed4544e174f1306d33eb4321c78
Ryan Palmer <tog54321@gmail.com>
2017-03-13 14:25:58 -0400

    V1, pushing changes and starting on the next version

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,18 @@
 import { assert } from 'chai';
 import GoogleAnalyticsSource from '../lib/index.js';
+import fs from 'fs';
+var config = {
+
+};
 
 describe('GoogleAnalyticsSource', function() {
-  it('', function() {
-  });
+    it('should make a request', function(done) {
+        const GA = new GoogleAnalyticsSource();
+        GA.onTask({}, config, '2017-03-06').then( function(data) {
+            console.log(data);
+            var x = fs.createWriteSteam('t.json');
+            x.write(data);
+            done();
+        });
+    });
 });
