e763f1d9598e281b9ddcb94ce18ffdec35963819
Ryan Palmer <tog54321@gmail.com>
2017-06-15 16:51:53 -0400

    updated to pull acct ids, updated to generate an empty row for every day requested

--- test/index.test.js
+++ test/index.test.js
@@ -1,65 +1,76 @@
 import { assert } from 'chai';
-import GoogleAnalyticsSource from '../lib/index.js';
-import fs from 'fs';
-import nock from 'nock';
-import config from './test-config';
 import moment from 'moment';
-import GoogleAuth from 'google-auth-library';
+import GoogleAnalyticsSource from '../lib/index';
+import config from './test-config';
 
-describe('GoogleAnalyticsSource', () =>  {
+describe('GoogleAnalyticsSource', () => {
     const GA = new GoogleAnalyticsSource();
     const time = moment().format();
     const TZ = moment.tz(time, config.timeZone);
 
-    describe('#UTC To Time Zone', () =>  {
-        it('shuld convert given time in utc to whatever timezone requested', () =>  {
+    describe('#ontask', () => {
+        it('should work', async (done) => {
+            console.log(await GA.onTask({}, config, TZ));
+            done();
+        })
+    })
+    describe('#UTC To Time Zone', () => {
+        it('shuld convert given time in utc to whatever timezone requested', () => {
             assert(GA.utcToTimeZone(time, config.timeZone).format() === TZ.format(), 'Generated Time is equal to time we know');
-        }); 
+        });
     });
 
-    describe('#Get Auth ', () =>  {
-        it('shuld use util file to generate authentication for api', () =>  {
+    describe('#Get Auth ', () => {
+        it('shuld use util file to generate authentication for api', () => {
             assert(GA.getAuth(config.connection), 'get auth should retrun oauth creds');
         });
     });
 
     const auth = GA.getAuth(config.connection);
+    let viewIds = config.viewIds;
+    describe('getAllViews', () => {
+        it('should request all view ids', async () => {
+            viewIds = await GA.getAllAccountViewIds(config.accIds, auth);
+            assert(viewIds);
+        });
+    });
 
-    describe('#Request Report', () =>  {
-        it('should make a request to googles api and return response', () =>  {
+    describe('#Request Report', () => {
+        it('should make a request to googles api and return response', () => {
             const requests = config.request;
-            requests[0].viewId = config.viewIds[0];
+            requests[0].viewId = viewIds[0];
             assert(GA.requestReport(requests[0], auth), 'makes a single request to googles endpoint');
         });
     });
 
-    describe('#Add Time Filter ', () =>  {
-        it('should take a list of requests and add time filters to their dimension filter clauses', () =>  {
+    describe('#Add Time Filter ', () => {
+        it('should take a list of requests and add time filters to their dimension filter clauses', () => {
             const resp = GA.addTimeFilter(config.request, TZ);
             assert(resp[0].dimensionFilterClauses[0].filters[0] !== undefined, 'compares added time filter');
         });
     });
 
-    describe('#Make Requests', () =>  {
-        it('should make requests to googles api and gather the results into an array', async () =>  {
+    describe('#Make Requests', () => {
+        it('should make requests to googles api and gather the results into an array', async () => {
             const requests = await config.request;
-            requests[0].viewId = config.viewIds[0];
-            const resp = await GA.makeRequest(requests,  auth);
+            requests[0].viewId = viewIds[0];
+            console.log(requests[0].viewId);
+            const resp = await GA.makeRequest(requests, auth);
             assert(resp[0].columnHeader !== undefined, 'There should be a response from googles api thats not undefined');
         });
-        it('should make a request with the list of viewIds',async  () => {
-            const reqs = await GA.viewRequestModifier(config,TZ);
+        it('should make a request with the list of viewIds', async () => {
+            const reqs = await GA.viewRequestModifier(viewIds, config.request, TZ, config.addTimeFilter);
             const resp = await GA.makeMultipleViewRequests(reqs, auth);
             assert(resp[0].columnHeader !== undefined, 'There should be a response from googles api thats not undefined');
         });
     });
 
-    describe('#Mod Reports', () =>  {
-        it('should add timestamp and request filters onto response reports for use in transform', async () =>  {
-            const requests = await  GA.addTimeFilter(config.request, TZ);
-            const reports = await GA.makeRequest(requests,  auth);
-            const conf = {request: requests};
-            const resp = await GA.modReports(conf, reports, TZ);
+    describe('#Mod Reports', () => {
+        it('should add timestamp and request filters onto response reports for use in transform', async () => {
+            const requests = await GA.addTimeFilter(config.request, TZ);
+            const reports = await GA.makeRequest(requests, auth);
+            const conf = { request: requests };
+            const resp = await GA.modReports(reports, TZ, config.queryLevel);
             assert(resp[0].timeStamp === TZ.format(), 'TimeStamp should be set');
             assert(resp[0].filters === requests[0].dimensionFilterClauses[0].filters, 'Filters should have been added');
         });
--- test/test-config.js
+++ test/test-config.js
@@ -3,36 +3,51 @@ require('dotenv').config();
 export default {
     addTimeFilter: false,
     timeZone: 'America/New_York',
-    viewIds: JSON.parse(process.env.VIEW_IDS),
-    connection:{
-        client: JSON.parse(process.env.CLIENT_CREDS),
-        oauth2: JSON.parse(process.env.OAUTH2)
-    },   
-    request: [
-    {
-        "dateRanges": [
-        {
-            "startDate": "today",
-            "endDate": "today"
-        }
-        ],
-        "dimensions": [
-        {
-            "name": "ga:adwordsCreativeID"
-        }
-        ],
-        "metrics": [
-        {
-            "expression": "ga:entrances"
-        },
-        {
-            "expression": "ga:goalAbandonsAll"
-        },
-        {
-            "expression": "ga:users"
-        }
-        ],
-        "includeEmptyRows": true
+    viewIds: ['151604012'],
+    connection: {
+        "client" : {
+            "client_id" : "140687279915-67jtr2gupb8i9olgrp94a3inoisefqlq.apps.googleusercontent.com", 
+            "project_id" : "astronomer-ga-2", 
+            "auth_uri" : "https://accounts.google.com/o/oauth2/auth", 
+            "token_uri" : "https://accounts.google.com/o/oauth2/token", 
+            "auth_provider_x509_cert_url" : "https://www.googleapis.com/oauth2/v1/certs", 
+            "client_secret" : "p1UFg_XrLYXWWkaNZMOFALRP", 
+            "redirect_uris" : [
+                "http://www.astronomer.io"
+            ]
+        }, 
+        "oauth2" : {
+            "access_token" : "ya29.GlsrBB6Z8vYQnwsAo3-V3yw5MZiESOVkok1t_X5QPeeCCWtr4yVHc1qzvGu0GRTzt6c8NWNb4cuSvS89aUHvMVMzBqYAqZE-ruprTrFDkn9xS7J8kWN7iMK4ZtYW", 
+            "refresh_token" : "1/05-9Al0voDIHYm1K66omdk4Yn_tJ92DmGNlE2KYQ4Yg", 
+            "token_type" : "Bearer", 
+            "expiry_date" : 1491968840950
+        }    
     },
-    ]
+    request: [
+        {
+            dateRanges: [
+                {
+                    startDate: '2017-01-01',
+                    endDate: 'yesterday',
+                },
+            ],
+            dimensions: [
+                {
+                    name: 'ga:date',
+                },
+                {
+                    name: 'ga:deviceCategory',
+                },
+                {
+                    name: 'ga:cityId',
+                },
+            ],
+            metrics: [
+                {
+                    expression: 'ga:sessions',
+                },
+            ],
+            includeEmptyRows: true,
+        },
+    ],
 };
